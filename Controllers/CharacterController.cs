db394db24d95a13914fa0b642f8714c1f3964903
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-03-29 16:58:24 +0200

    Get first character of the list and GetAll route

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotnet7_rpg.Controllers;
 
 // Controllerbase is a base class for an MVC controller without View support, that will be Controller
-[ApiController]      // Indicates that a type and all derived types are used to serve HTTP API responses.
-[Route("api/[controller]")] // "api/Character" the part of the name of c# class that comes before Controller word
+// Indicates that a type and all derived types are used to serve HTTP API responses.
+// "api/Character" the part of the name of c# class that comes before Controller word
+[ApiController]      
+[Route("api/[controller]")] 
 public class CharacterController : ControllerBase
 {
     private static List<Character> characters = new List<Character>
@@ -14,9 +16,16 @@ public class CharacterController : ControllerBase
         new Character { Name = "Sam" }
     };
     
-    [HttpGet]
-    public ActionResult<List<Character>> Get() // ActionResult<Character> means that this action returns a Character type data
+    // api will be: "api/Character/GetAll", alternative of [HttpGet("GetAll")] could be: [HttpGet] and underneath: [Route("GetAll")]
+    [HttpGet("GetAll")] 
+    public ActionResult<List<Character>> Get() 
     {
         return Ok(characters); // ok = 200 status code, NotFound = 404, BadRequest = 400
+    }
+    
+    [HttpGet]
+    public ActionResult<Character> GetSingle() 
+    {
+        return Ok(characters[0]); 
     }
 }
