48f3d02a6184acadfd422973a539a2738d5a5502
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-04-05 15:27:51 +0200

    Data Seeding - Add Skills to the Database - Data seeding in the context of Entity Framework (EF) refers to the process of providing initial data for the database when it is created or updated. This is useful for populating the database with default or sample data during application development, testing, or deployment. - In your DbContext class, you can override the OnModelCreating method and use the ModelBuilder to define the initial data for your entities. This approach is useful when you want to seed data during the creation or migration of the database schema. Seeded data will be inserted into the database only if the table is empty. - "dotnet ef migrations add SkillSeeding" - "dotnet ef database update"

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,7 +8,16 @@ public class DataContext : DbContext
     {
         
     }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Skill>().HasData(
+            new Skill {Id = 1, Name="Fireball", Damage = 30},
+            new Skill {Id = 2, Name="Frenzy", Damage = 20},
+            new Skill {Id = 3, Name="Blizzard", Damage = 50}
+        );
+    }
+
     // with DbSet we are able to query and save RPG charcaters 
     // the name of DbSet will be the pluralized name of the Character entity (and it will be the corrisponding database table)
     // this is the Characters table
