b62c98756e8f544fb46d56d8d8401d9658dbb0ee
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-04-05 14:58:42 +0200

    Many-To-Many Relationship with Skills

--- Models/Character.cs
+++ Models/Character.cs
@@ -3,7 +3,7 @@ namespace dotnet7_rpg.Models;
 public class Character
 {
     public int Id { get; set; } 
-    public string Name { get; set; } = "Frodo;";
+    public string Name { get; set; } = "Frodo";
     public int HitPoints { get; set; } = 100;
     public int Strength { get; set; } = 10;
     public int Defense { get; set; } = 10;
@@ -11,4 +11,5 @@ public class Character
     public RpgClass Class { get; set; } = RpgClass.Knight;
     public User? User { get; set; }
     public Weapon? Weapon { get; set; }
+    public List<Skill>? Skills { get; set; }
 }
